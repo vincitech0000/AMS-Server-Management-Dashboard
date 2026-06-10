6cdb2e69966f8c5689be7fee7633f301ee6e4158
Vinci tech <vincitech0000@gmail.com>
2026-06-10 16:51:47 +0000

    add logo in ASTPP box, logo reference https://encrypted-tbn0.gstatic.com

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   serverExternalPackages: [
